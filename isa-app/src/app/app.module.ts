f6f18f3d04ca55e5e85e15a51281cd62e13f320f
Asshai <andjela.jo998@gmail.com>
2020-06-01 22:08:41 +0200

    Initial components
    Generating core components

--- isa-app/src/app/app.module.ts
+++ isa-app/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { SuccessComponent } from './auth/success/success.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { GameplayComponent } from './gameplay/gameplay.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    SuccessComponent,
+    WelcomeComponent,
+    GameplayComponent,
+    CampaignsComponent
   ],
   imports: [
     BrowserModule
---
+++ isa-app/src/app/gameplay/gameplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameplay',
+  templateUrl: './gameplay.component.html',
+  styleUrls: ['./gameplay.component.css']
+})
+export class GameplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
